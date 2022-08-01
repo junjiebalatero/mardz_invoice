eceb6d1d8fef07fee8f82020826914282d93804e
junjiebalatero <junjiebalatero@gmail.com>
2022-08-02 01:28:14 +0800

    first commit

---
+++ database/mardz_invoice.sql
@@ -0,0 +1,145 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 01, 2022 at 07:16 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mardz_invoice`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_order`
+--
+
+CREATE TABLE `invoice_order` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `order_receiver_name` varchar(250) NOT NULL,
+  `order_receiver_address` text NOT NULL,
+  `order_total_before_tax` decimal(10,2) NOT NULL,
+  `order_total_tax` decimal(10,2) NOT NULL,
+  `order_tax_per` varchar(250) NOT NULL,
+  `order_total_after_tax` double(10,2) NOT NULL,
+  `order_amount_paid` decimal(10,2) NOT NULL,
+  `order_total_amount_due` decimal(10,2) NOT NULL,
+  `note` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `invoice_order`
+--
+
+INSERT INTO `invoice_order` (`order_id`, `user_id`, `order_date`, `order_receiver_name`, `order_receiver_address`, `order_total_before_tax`, `order_total_tax`, `order_tax_per`, `order_total_after_tax`, `order_amount_paid`, `order_total_amount_due`, `note`) VALUES
+(2, 123456, '2022-07-31 11:33:42', 'PCpl. Xerxes Bon G. Lumacad ', 'Customer (No Name Given)\r\n0905 901 9941\r\nBacolod, Lanao Del Norte', '3500.00', '0.00', '0', 3500.00, '3500.00', '0.00', 'Pending transaction');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_order_item`
+--
+
+CREATE TABLE `invoice_order_item` (
+  `order_item_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `item_code` varchar(250) NOT NULL,
+  `item_name` varchar(250) NOT NULL,
+  `order_item_quantity` decimal(10,2) NOT NULL,
+  `order_item_price` decimal(10,2) NOT NULL,
+  `order_item_final_amount` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `invoice_order_item`
+--
+
+INSERT INTO `invoice_order_item` (`order_item_id`, `order_id`, `item_code`, `item_name`, `order_item_quantity`, `order_item_price`, `order_item_final_amount`) VALUES
+(4383, 2, '13555', 'Tombstone Granite 40cm x 50cm with flag image', '1.00', '3500.00', '3500.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_user`
+--
+
+CREATE TABLE `invoice_user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `mobile` bigint(20) NOT NULL,
+  `address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `invoice_user`
+--
+
+INSERT INTO `invoice_user` (`id`, `email`, `password`, `first_name`, `last_name`, `mobile`, `address`) VALUES
+(123456, 'margaritobalatero@gmail.com', 'MardzInvoice1954/', 'Margarito V.', 'Balatero Jr.', 9109652004, 'Branch Address:\r\nInfront Sto. Nino High School,\r\nPurok 6, Poblacion 2, Bacolod\r\nLanao Del Norte, Mindanao\r\nPhilippines\r\n\r\n');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `invoice_order`
+--
+ALTER TABLE `invoice_order`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `invoice_order_item`
+--
+ALTER TABLE `invoice_order_item`
+  ADD PRIMARY KEY (`order_item_id`);
+
+--
+-- Indexes for table `invoice_user`
+--
+ALTER TABLE `invoice_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `invoice_order`
+--
+ALTER TABLE `invoice_order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=683;
+
+--
+-- AUTO_INCREMENT for table `invoice_order_item`
+--
+ALTER TABLE `invoice_order_item`
+  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4384;
+
+--
+-- AUTO_INCREMENT for table `invoice_user`
+--
+ALTER TABLE `invoice_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123457;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
