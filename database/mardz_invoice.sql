484af3f27cdc9d0ddfbbe9d6bcbf00d5f5b088bb
junjiebalatero <junjiebalatero@gmail.com>
2023-10-08 21:06:30 +0800

    ok

--- database/mardz_invoice.sql
+++ database/mardz_invoice.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 01, 2022 at 07:16 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Oct 08, 2023 at 03:02 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,14 +40,17 @@ CREATE TABLE `invoice_order` (
   `order_amount_paid` decimal(10,2) NOT NULL,
   `order_total_amount_due` decimal(10,2) NOT NULL,
   `note` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `invoice_order`
 --
 
 INSERT INTO `invoice_order` (`order_id`, `user_id`, `order_date`, `order_receiver_name`, `order_receiver_address`, `order_total_before_tax`, `order_total_tax`, `order_tax_per`, `order_total_after_tax`, `order_amount_paid`, `order_total_amount_due`, `note`) VALUES
-(2, 123456, '2022-07-31 11:33:42', 'PCpl. Xerxes Bon G. Lumacad ', 'Customer (No Name Given)\r\n0905 901 9941\r\nBacolod, Lanao Del Norte', '3500.00', '0.00', '0', 3500.00, '3500.00', '0.00', 'Pending transaction');
+(2, 123456, '2021-01-31 14:03:42', 'abcd', 'Admin\r\nA - 4000, Ashok Nagar, New Delhi,\r\n 110096 India.\r\n12345678912\r\nadmin@phpzag.com', 342400.00, 684800.00, '200', 1027200.00, 45454.00, 981746.00, 'this note txt'),
+(682, 123456, '2021-08-19 15:13:36', 'ABCD pvt ltd', 'New Delhi India', 750000.00, 7500.00, '1', 757500.00, 20000.00, 737500.00, 'this is a note'),
+(683, 123456, '2021-08-19 16:54:15', 'XYZ', 'Newyork USA', 1320000.00, 26400.00, '2', 1346400.00, 20000.00, 1326400.00, 'some note'),
+(684, 123456, '2023-10-08 12:52:28', 'Jay Agawin', 'Bacolod Lanao Del Norte', 7300.00, 0.00, '', 7300.00, 7300.00, 0.00, '');
 
 -- --------------------------------------------------------
 
@@ -63,14 +66,24 @@ CREATE TABLE `invoice_order_item` (
   `order_item_quantity` decimal(10,2) NOT NULL,
   `order_item_price` decimal(10,2) NOT NULL,
   `order_item_final_amount` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `invoice_order_item`
 --
 
 INSERT INTO `invoice_order_item` (`order_item_id`, `order_id`, `item_code`, `item_name`, `order_item_quantity`, `order_item_price`, `order_item_final_amount`) VALUES
-(4383, 2, '13555', 'Tombstone Granite 40cm x 50cm with flag image', '1.00', '3500.00', '3500.00');
+(4100, 2, '13555', 'Face Mask', 120.00, 2000.00, 240000.00),
+(4101, 2, '34', 'mobile', 10.00, 10000.00, 100000.00),
+(4102, 2, '34', 'mobile battery', 1.00, 34343.00, 34343.00),
+(4103, 2, '34', 'mobile cover', 10.00, 200.00, 2000.00),
+(4104, 2, '36', 'testing', 1.00, 2400.00, 2400.00),
+(4364, 682, '123456', 'iphone 6s', 12.00, 25000.00, 300000.00),
+(4365, 682, '345678', 'one plus', 10.00, 45000.00, 450000.00),
+(4368, 683, '00123', 'iphone 12', 10.00, 80000.00, 800000.00),
+(4369, 683, '00124', 'iphone 8', 13.00, 40000.00, 520000.00),
+(4370, 684, '1', 'Tombstone 40cm x 50cm with image', 1.00, 3800.00, 3800.00),
+(4371, 684, '2', 'Tombstone 40cm x 50cm ', 1.00, 3500.00, 3500.00);
 
 -- --------------------------------------------------------
 
@@ -86,14 +99,14 @@ CREATE TABLE `invoice_user` (
   `last_name` varchar(100) NOT NULL,
   `mobile` bigint(20) NOT NULL,
   `address` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `invoice_user`
 --
 
 INSERT INTO `invoice_user` (`id`, `email`, `password`, `first_name`, `last_name`, `mobile`, `address`) VALUES
-(123456, 'EMAIL-HERE@gmail.com', 'PASSWORD HERE', 'Margarito V.', 'Balatero Jr.', MOBILE HERE, 'Branch Address:\r\nInfront Sto. Nino High School,\r\nPurok 6, Poblacion 2, Bacolod\r\nLanao Del Norte, Mindanao\r\nPhilippines\r\n\r\n');
+(123456, 'margaritobalatero@gmail.com', 'junjie', 'Admin', '', 9603075963, 'Infront Sto. Niño High School\r\nPurok 6 Poblacion 2 Bacolod\r\nLanao Del Norte');
 
 --
 -- Indexes for dumped tables
@@ -125,13 +138,13 @@ ALTER TABLE `invoice_user`
 -- AUTO_INCREMENT for table `invoice_order`
 --
 ALTER TABLE `invoice_order`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=683;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=685;
 
 --
 -- AUTO_INCREMENT for table `invoice_order_item`
 --
 ALTER TABLE `invoice_order_item`
-  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4384;
+  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4372;
 
 --
 -- AUTO_INCREMENT for table `invoice_user`
